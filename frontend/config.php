f987244d30e5b0ebb72330d8aeb364a276b9ff47
Dani Bednarski <bedz@Danis-MacBook-Pro.local>
2020-12-07 23:06:39 +1100

    Initial Commit

---
+++ frontend/config.php
@@ -0,0 +1,63 @@
+<?php 
+// CONFIG - These control the look and details on your site. Consult documentation for more details.
+
+// GENERAL
+
+// Site URL (no trailing slash)
+define('siteURL', $YOURLS_SITE);
+
+// Page title for your site
+define('title', 'bedz.me'); 
+
+// The short title of your site, used in the footer and in some sub pages
+define('shortTitle', 'bedz.me');
+
+// A description of your site, shown on the homepage.
+define('description', ''); 
+
+// The favicon for your site
+define('favicon', '/favicon.ico');
+
+// Logo for your site, displayed on home page
+define('logo', '/logo.jpg');
+
+// Enable reCAPTCHA V3
+// It is highly recommended you use reCAPTCHA V3. It will stop spam. You can get a site and secret key from here: https://www.google.com/recaptcha/admin/create
+define("enableRecaptcha", true);
+
+// reCAPTCHA V3 Site Key
+define("recaptchaV3SiteKey", '6LeVHAEVAAAAALeuHFepsGHhlHp7iX7jLNUSJkTU');
+
+// reCAPTCHA V3 Secret Key
+define("recaptchaV3SecretKey", '6LeVHAEVAAAAAKaC22HD9CQpyyuvk3cfPDE9c1Gr');
+
+// Enables the custom URL field
+// true or false
+define('enableCustomURL', true);
+
+// Optional
+// Set a primary colour to be used. Default: #007bff
+// Here are some other colours you could try:
+// #f44336: red, #9c27b0: purple, #00bcd4: teal, #ff5722: orange
+define('colour', '#000000');
+
+// Optional
+// Set a background image to be used.
+// default: unsplash.com random daily photo of the day
+// More possibilities of photo embedding from unsplash could be found at: https://source.unsplash.com
+// define('backgroundImage', 'https://source.unsplash.com/daily');
+
+// FOOTER
+
+// These are the links in the footer. Add a new link for each new link.
+// The array follows a title link structure:
+// "TITLE" => "LINK",
+/* $footerLinks = [
+    "About"   =>  "https://sleeky.flynntes.com/",
+    "Contact" =>  "https://sleeky.flynntes.com/",
+    "Legal"   =>  "https://sleeky.flynntes.com/",
+    "Admin"   =>  "/admin"
+];
+ */
+
+?>
